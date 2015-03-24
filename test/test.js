5246c0909e9a650cfe4e32b19875006280efcd2a
Marco Godínez <markotom@gmail.com>
2015-03-24 17:24:59 -0600

    Create Zoho and Zoho.Invoice functions

--- test/test.js
+++ test/test.js
@@ -1,4 +1,25 @@
 var assert = require('assert'),
     sinon  = require('sinon'),
+    config = require('./config'),
     Zoho   = require('../lib');
 
+var zohoInvoice = new Zoho.Invoice(config.invoice);
+
+// Zoho
+describe('Zoho', function () {
+  it('Zoho should be a function', function () {
+    assert.equal(typeof Zoho, 'function');
+  });
+});
+
+// Zoho Invoice
+describe('Zoho Invoice', function () {
+  it('zohoInvoice should be an instance of Zoho.Invoice', function () {
+    assert(zohoInvoice instanceof Zoho.Invoice);
+  });
+
+  it('zohoInvoice should have this properties', function () {
+    assert(zohoInvoice.authtoken);
+    assert.equal(zohoInvoice.authtoken, config.invoice.authtoken);
+  });
+});
