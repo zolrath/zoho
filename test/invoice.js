096d95bf3eaaab5d68be3b8990d8753fddbeceba
Marco Godínez <markotom@gmail.com>
2015-03-24 19:46:43 -0600

    Added deleteRecord method to Zoho Invoice module

--- test/invoice.js
+++ test/invoice.js
@@ -30,6 +30,9 @@ describe('Zoho Invoice', function () {
     assert.equal(typeof zohoInvoice._request, 'function');
   });
 
+  // To assign created id;
+  var created_id;
+
   describe('Zoho Requests', function () {
     beforeEach(function () {
       this.callback = sinon.spy();
@@ -73,6 +76,8 @@ describe('Zoho Invoice', function () {
         assert.equal(error, null); // No response errors
         assert.equal(typeof response, 'object'); // Response
         assert.equal(response.code, 0); // No errors
+
+        created_id = response.contact.contact_id;
 
         done();
       }.bind(this.callback), 500);
@@ -134,5 +139,41 @@ describe('Zoho Invoice', function () {
         done();
       }.bind(this.callback), 500);
     });
+  });
+
+  describe('Delete Zoho Invoice records', function () {
+    beforeEach(function () {
+      this.callback = sinon.spy();
+    });
+
+    it('should delete a contact', function (done) {
+      zohoInvoice.deleteRecord('contacts', created_id, this.callback);
+
+      setTimeout(function () {
+        assert(this.calledOnce);
+
+        var error = this.args[0][0],
+            response = this.args[0][1];
+
+        assert.equal(error, null); // No response errors
+        assert.equal(typeof response, 'object'); // Response
+        assert.equal(response.code, 0); // No errors
+
+        done();
+      }.bind(this.callback), 500);
+    });
+
+    it('should fail when trying delete a contact with id param missing', function () {
+      zohoInvoice.deleteRecord('contacts', undefined, this.callback);
+      assert(this.callback.calledOnce);
+      assert.notEqual(this.callback.args[0][0], null);
+      zohoInvoice.deleteRecord('contacts', {}, this.callback);
+      assert(this.callback.calledTwice);
+      assert.notEqual(this.callback.args[1][0], null);
+      zohoInvoice.deleteRecord('contacts', null, this.callback);
+      assert(this.callback.calledThrice);
+      assert.notEqual(this.callback.args[2][0], null);
+    });
+
   });
 });
