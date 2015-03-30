ec05c66c31e1baa742577063384d4e0148ee84f8
Marco Godínez <markotom@gmail.com>
2015-03-30 16:21:53 -0600

    Execute tests for each module

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp  = require('gulp'),
     mocha = require('gulp-mocha');
 
+var modules = ['crm', 'invoice', 'support'];
+
 gulp.task('test', function () {
   return gulp.src(['./test/**/*.js'], { read: false })
     .pipe(mocha({ reporter: 'dot' }));
 });
 
+modules.forEach(function (module) {
+  gulp.task('test:' + module, function () {
+    return gulp.src(['./test/'+ module + '.js'], { read: false })
+      .pipe(mocha({ reporter: 'dot' }));
+  });
+});
+
 gulp.task('default', function () {
-  gulp.watch(['./lib/**/*', './test/**/*'], ['test']);
+  modules.forEach(function (module) {
+    gulp.watch(['./lib/' + module + '.js', './test/' + module + '.js'], ['test:' + module]);
+  });
 });
