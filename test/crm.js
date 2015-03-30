852d47e385f85dc62ebf99ce233032d1428d4ada
Marco Godínez <markotom@gmail.com>
2015-03-30 16:35:45 -0600

    Zoho CRM is able to make requests to server

--- test/crm.js
+++ test/crm.js
@@ -1,4 +1,5 @@
 var assert = require('assert'),
+    sinon  = require('sinon'),
     config = require('./config'),
     Zoho   = require('../lib');
 
@@ -30,5 +31,28 @@ describe('Zoho CRM', function () {
 
   it('zohoCRM should have this private functions', function () {
     assert.equal(typeof zohoCRM._request, 'function');
+  });
+
+  describe('Zoho Requests', function () {
+    beforeEach(function () {
+      this.callback = sinon.spy();
+    });
+
+    it('should be able to make requests to Zoho server', function (done) {
+      zohoCRM._request('GET', 'fakeroute', {}, this.callback);
+
+      setTimeout(function () {
+        assert(this.calledOnce);
+
+        var error = this.args[0][0], response = this.args[0][1];
+
+        assert.equal(response, null);
+        assert.equal(typeof error, 'object');
+        assert.equal(error.code, 4600);
+        assert(/Unable to process your request/.test(error.message));
+
+        done();
+      }.bind(this.callback), 500);
+    });
   });
 });
